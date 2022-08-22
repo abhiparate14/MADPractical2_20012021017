75b237df88d209ae5fb69588f7b7c0168ecfdb34
tejasvi <pateltejasvi329@gmail.com>
2022-08-22 12:28:31 +0530

    Initial commit

---
+++ app/src/main/java/com/example/madpractical2_20012021017/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.madpractical2_20012021017
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+    private lateinit var mycl:ConstraintLayout
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mycl=findViewById(R.id.mycl)
+        Log.i(TAG, "onCreate: OnCreate Method is called")
+        Toast.makeText(
+            this,
+            "onCreate: OnCreate Method is called",
+            Toast.LENGTH_LONG
+        ).show()
+        Snackbar.make(
+            mycl,
+            R.string.oncreatemsg,
+            Snackbar.LENGTH_SHORT
+        ).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mycl=findViewById(R.id.mycl)
+        Log.i(TAG, "onStart: OnStart Method is called")
+        Toast.makeText(
+            this,
+            "onStart: OnStart Method is called",
+            Toast.LENGTH_LONG
+        ).show()
+        Snackbar.make(
+            mycl,
+            R.string.onstartmsg,
+            Snackbar.LENGTH_SHORT
+        ).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mycl=findViewById(R.id.mycl)
+        Log.i(TAG, "onResume: OnResume Method is called")
+        Toast.makeText(
+            this,
+            "onResume: Onresume Method is called",
+            Toast.LENGTH_LONG
+        ).show()
+        Snackbar.make(
+            mycl,
+            R.string.onresumemsg,
+            Snackbar.LENGTH_SHORT
+        ).show()
+    }
+}
